7d18d6b91a6d6118f2a619bcfc397e8d3b252207
parsa <parsafarinnia@gmail.com>
2017-05-12 14:10:38 +0430

    a

--- src/aut/CardDeck.java
+++ src/aut/CardDeck.java
@@ -4,4 +4,17 @@ package aut;
  * Created by parsa on 5/12/17.
  */
 public class CardDeck extends  ObjectDeck{
+    public int number;
+    public Card [] cards;
+
+    public CardDeck(Card[] cards) {
+        this.cards = cards;
+    }
+
+    @Override
+    public Card deal(){
+    Card card=cards[number];
+    number--;
+return card;
+    }
 }
